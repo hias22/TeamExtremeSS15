29971493433b380787d653058c6c98892c783752
mathiasglatz <m.glatz@student.tugraz.at>
2015-05-02 12:27:43 +0200

    [MG, MM] created first testcases (userinterface) and created first interface (userinterface)

---
+++ app/src/androidTest/java/com/example/moja/pfa/uitest/EnteringScreenTestUI.java
@@ -0,0 +1,40 @@
+package com.example.moja.pfa.uitest;
+
+import android.test.ActivityInstrumentationTestCase2;
+
+import com.example.moja.pfa.EnteringScreen;
+import com.robotium.solo.Solo;
+
+import junit.framework.TestCase;
+
+/**
+ * Created by Mathias on 02.05.2015.
+ */
+public class EnteringScreenTestUI extends ActivityInstrumentationTestCase2 {
+
+    private Solo mySolo;
+
+    public EnteringScreenTestUI() {
+        super(EnteringScreen.class);
+    }
+
+    public void setUp() throws Exception {
+        super.setUp();
+        mySolo = new Solo(getInstrumentation(), getActivity());
+
+    }
+
+    public void tearDown() throws Exception {
+
+    }
+
+    public void testenterValidInputInTextfields() {
+        mySolo.enterText(4, "1000.00");
+        mySolo.enterText(3, "Spar");
+    }
+
+    public void testenterInvalidInputInTextfields() {
+        mySolo.enterText(1, "abcde");
+        mySolo.enterText(2, "Spar");
+    }
+}
